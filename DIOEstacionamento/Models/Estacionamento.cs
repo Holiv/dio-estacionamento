867129f4c6151d77b64ed2906dc5dead51ed8ddd
holiv <mrheltonso@gmail.com>
2023-10-12 23:51:55 -0300

    first commit

---
+++ DIOEstacionamento/Models/Estacionamento.cs
@@ -0,0 +1,70 @@
+﻿using System;
+namespace DIOEstacionamento.Models
+{
+	public class Estacionamento
+	{
+        private readonly decimal precoInicial;
+        private readonly decimal precoPorHora;
+        private List<string> veiculos = new();
+
+        public Estacionamento(decimal precoInicial, decimal precoPorHora)
+		{
+            this.precoInicial = precoInicial;
+            this.precoPorHora = precoPorHora;
+        }
+
+        public void AdicionarVeiculo()
+        {
+            Console.WriteLine("Digite a placa do Veículo");
+            try
+            {
+                veiculos.Add(Console.ReadLine().ToUpper());
+                Console.WriteLine("Veículo adicionado com sucesso");
+            }
+            catch
+            {
+                Console.WriteLine("Placa inválida");
+            }
+            
+        }
+
+        public void RemoverVeiculo()
+        {
+            Console.WriteLine("Digite a placa do carro a ser removido: ");
+            var placa = Console.ReadLine().ToUpper();
+
+            if(veiculos.Contains(placa))
+            {
+                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado");
+                int horas = int.Parse(Console.ReadLine());
+                decimal valorTotal = precoInicial + (horas * precoPorHora);
+
+                veiculos.Remove(placa);
+
+                Console.WriteLine($"O veículo de placa {placa} foi removido e o valor total foi de {valorTotal}");
+            }
+            else
+            {
+                Console.WriteLine("Desculpe, este veículo não está estacionado aqui. Verifique se digitou a placa corretamente");
+            }
+        }
+
+        public void ListarVeiculos()
+        {
+            if (veiculos.Any())
+            {
+                Console.WriteLine("Os veículos estacionados aqui são:");
+                foreach (var veiculo in veiculos)
+                {
+                    Console.WriteLine($"{veiculo}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não há veículos estacionados.");
+            }
+        }
+
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# DIO Challenge - Estacionamento
+
+Aplicação criada para atender ao desafio da DIO - Construindo uma aplicação para simular operações de um estacionamento utilizando C# (Console).
